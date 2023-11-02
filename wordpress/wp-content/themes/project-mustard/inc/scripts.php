82deb9ccc4112ac4aa7211dc82b5079b5ee67cdf
Carl <me@mrcarllister.co.uk>
2023-11-02 10:40:00 +0000

    updated hammerjs cdn

--- wordpress/wp-content/themes/project-mustard/inc/scripts.php
+++ wordpress/wp-content/themes/project-mustard/inc/scripts.php
@@ -5,7 +5,7 @@ function pm_scripts__enqueue_script() {
     wp_deregister_script('jquery');
 
 	wp_enqueue_script('jQuery', 'https://code.jquery.com/jquery-3.7.1.min.js', array(), null, true);
-	wp_register_script('hammerJS', 'https://hammerjs.github.io/dist/hammer.min.js', array('jQuery'));
+	wp_register_script('hammerJS', 'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js', array('jQuery'));
 	wp_register_script('carouselJS', get_template_directory_uri() . '/assets/js/carousel.js', array('jQuery', 'hammerJS'));
 
 }
